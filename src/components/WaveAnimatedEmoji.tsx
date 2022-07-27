cd0f595158a41937bbb819b46055f23158c21ff2
kadaradam <xadam1996@gmail.com>
2022-07-27 09:55:37 +0200

    feat: AddWaveAnimatedEmoji

---
+++ src/components/WaveAnimatedEmoji.tsx
@@ -0,0 +1,34 @@
+import { css, keyframes } from '@emotion/react';
+
+interface AnimatedWaveProps {
+	children: React.ReactNode;
+}
+
+const waveEffect = keyframes`
+    0% { transform: rotate(0.0deg); }
+    15 { transform: rotate(14.0deg); }
+    30% { transform: rotate(-8.0deg); }
+    40% { transform: rotate(14.0deg); }
+    50% { transform: rotate(-4.0deg); }
+    60% { transform: rotate(10.0deg); }
+    70% { transform: rotate(0.0deg); }
+    100% { transform: rotate(0.0deg); }
+`;
+
+const WaveAnimatedEmoji = ({ children }: AnimatedWaveProps) => {
+	return (
+		<span
+			css={css`
+				animation-name: ${waveEffect};
+				animation-duration: 2.5s;
+				animation-iteration-count: infinite;
+				transform-origin: 70% 70%;
+				display: inline-block;
+			`}
+		>
+			{children}
+		</span>
+	);
+};
+
+export default WaveAnimatedEmoji;

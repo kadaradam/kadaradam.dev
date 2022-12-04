6887acc5c2c66cc2af9438f7df8865cbfd2846a7
kadaradam <xadam1996@gmail.com>
2022-12-04 20:48:32 +0100

    fix: show toggle theme on mobile too

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -11,7 +11,7 @@ const ToggleThemeButton = () => {
 	};
 
 	return (
-		<Box sx={{ display: { xs: 'none', md: 'inline-block' } }}>
+		<Box sx={{ display: 'inline-block' }}>
 			<IconButton
 				onClick={handleThemeToggle}
 				aria-label="toggle-theme"
